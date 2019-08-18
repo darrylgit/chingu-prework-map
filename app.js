728495612bf90cb2febe889adb838f9deeded6e5
darrylgit <darrylsmithup@gmail.com>
2019-08-18 16:10:31 -0400

    Refactor sidebar toggle

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 $(document).ready(function() {
-  // Change toggle button appearance when clicked
+  // Sidebar toggle variables
+  const openerIcon = "fa-angle-double-right";
+  const closerIcon = "fa-angle-double-left";
+  const toggleSpeed = 300;
+  const visibleEdge = 15;
+  let leftWhenHidden = (function(edge) {
+    let units = $("#sidebar")
+      .css("width")
+      .match(/\D/g)
+      .join("");
+    let widthValue = $("#sidebar")
+      .css("width")
+      .match(/\d+(?:\.\d+)?/g)
+      .map(Number);
+    widthValue[0] -= edge;
+    return "-" + widthValue[0].toString() + units;
+  })(visibleEdge);
+  $("#sidebar").css("left", leftWhenHidden);
+
+  // Toggle sidebar
   $("#toggle").click(function() {
-    if ($("#toggleIcon").attr("class") == "fas fa-angle-double-right") {
+    if ($("#toggleIcon").attr("class") == "fas " + openerIcon) {
+      //setHiddenValue();
       $("#toggleIcon")
-        .removeClass("fa-angle-double-right")
-        .addClass("fa-angle-double-left");
-      $("#sidebar").animate({ left: 0 }, 500);
-    } else if ($("#toggleIcon").attr("class") == "fas fa-angle-double-left") {
+        .removeClass(openerIcon)
+        .addClass(closerIcon);
+      $("#sidebar").animate({ left: 0 }, toggleSpeed);
+    } else if ($("#toggleIcon").attr("class") == "fas " + closerIcon) {
       $("#toggleIcon")
-        .removeClass("fa-angle-double-left")
-        .addClass("fa-angle-double-right");
-      $("#sidebar").animate({ left: "-320px" }, 500);
+        .removeClass(closerIcon)
+        .addClass(openerIcon);
+      //$("#sidebar").animate({ left: sidebarHidden(visibleEdge) }, toggleSpeed);
+      $("#sidebar").animate({ left: leftWhenHidden }, toggleSpeed);
     }
   });
 });
